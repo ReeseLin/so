b8c361b144a02eae251ed994e60309ae62c664dd
ReeseLin <172053362@qq.com>
2016-04-09 15:54:24 +0800

    1，修改获取消息和发送消息中的数据操作

--- so/src/cn/lb/service/impl/GetChatRoomMsg.java
+++ so/src/cn/lb/service/impl/GetChatRoomMsg.java
@@ -1,6 +1,5 @@
 package cn.lb.service.impl;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import cn.lb.bean.QueryMsg;
@@ -19,63 +18,35 @@ import cn.lb.utils.DBUtils;
  */
 public class GetChatRoomMsg extends MainService {
 
-	public static final String SQL_CHECK_MEMBER_ISCREATER = " SELECT isagree FROM chat_room_member WHERE chatroomid=:chatroomid AND userid=:userid;";
+	// public static final String SQL_CHECK_MEMBER_ISCREATER =
+	// " SELECT isagree FROM chat_room_member WHERE chatroomid=:chatroomid AND userid=:userid;";
 
-	public static final String SQL_GET_CHAT_ROOM_MESSAGE = "SELECT * FROM chat_room_message WHERE createtime >:lasttime AND senderid!=:userid AND chatroomid=:chatroomid;";
+	// public static final String SQL_GET_CHAT_ROOM_MESSAGE =
+	// "SELECT * FROM chat_room_message WHERE createtime >:lasttime AND senderid!=:userid ;";
+
+	public static final String SQL_GET_MESSAGE = "SELECT * FROM chat_room_message "
+			+ " WHERE chatroomid "
+			+ " IN (SELECT chatroomid FROM chat_room_member WHERE userid=:userid AND isagree='1')"
+			+ " AND senderid !=:userid AND createtime >:lasttime ORDER BY createtime;";
 
 	@Override
 	public QueryMsg execute() throws Exception {
 		Map<String, Object> map = dataTable.get(0);
 
-		String userid = (String) map.get("userid");
-		String lasttime = (String) map.get("lasttime");
-		String chatroomid = (String) map.get("chatroomid");
+		SQLQueryMsg sqlQueryMsg = null;
 
-		SQLQueryMsg sqlQueryMsg = new SQLQueryMsg();
-		Map<String, Object> parameters = new HashMap<String, Object>();
-		parameters.put("userid", userid);
-		parameters.put("lasttime", lasttime);
-		parameters.put("chatroomid", chatroomid);
-		sqlQueryMsg.setSql(SQL_CHECK_MEMBER_ISCREATER);
-		sqlQueryMsg.setParameters(parameters);
-
-		// 执行
+		sqlQueryMsg = new SQLQueryMsg();
+		sqlQueryMsg.setSql(SQL_GET_MESSAGE);
+		sqlQueryMsg.setParameters(map);
 		DBUtils.executeSQL(sqlQueryMsg);
-		String isagree=null;
-		if(sqlQueryMsg.getResultMsg().size()!=0){
-			 isagree = (String) sqlQueryMsg.getResultMsg().get(0)
-					.get("isagree");
-		}else{
-			return setErrorResQueryMsg();
-		}
-		
-		
-		if ("1".equals(isagree)) {
-			sqlQueryMsg = new SQLQueryMsg();
-			sqlQueryMsg.setSql(SQL_GET_CHAT_ROOM_MESSAGE);
-			sqlQueryMsg.setParameters(parameters);
-			DBUtils.executeSQL(sqlQueryMsg);
-		}else{
-			return setErrorResQueryMsg();
-		}
 
 		// 构建回应数据
 		QueryMsg resQueryMsg = new QueryMsg();
 		resQueryMsg.setResponse(true);
 		resQueryMsg.setResult("0");
-		if(sqlQueryMsg.getResultMsg()!=null){
+		if (sqlQueryMsg.getResultMsg() != null) {
 			resQueryMsg.setDataTable(sqlQueryMsg.getResultMsg());
 		}
 		return resQueryMsg;
-
 	}
-
-	private QueryMsg setErrorResQueryMsg() {
-		QueryMsg resQueryMsg = new QueryMsg();
-		resQueryMsg.setResponse(true);
-		resQueryMsg.setResult("1");
-		resQueryMsg.setError("未加入该房间！");
-		return resQueryMsg;
-	}
-
 }
--- so/src/cn/lb/service/impl/SendChatRoomMsg.java
+++ so/src/cn/lb/service/impl/SendChatRoomMsg.java
@@ -1,6 +1,7 @@
 package cn.lb.service.impl;
 
-import java.util.HashMap;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 import cn.lb.bean.QueryMsg;
@@ -21,36 +22,34 @@ public class SendChatRoomMsg extends MainService {
 
 	public static final String SQL_CHECK_MEMBER_ISCREATER = " SELECT isagree FROM chat_room_member WHERE chatroomid=:chatroomid AND userid=:userid;";
 
-	public static final String SQL_UPDATE_CHAT_ROOM_MESSAGE = " INSERT INTO chat_room_message (chatroomid,message,senderid,createtime,sendername) VALUES (:chatroomid,:message,:userid, NOW(),:sendername);";
-
+	public static final String SQL_UPDATE_CHAT_ROOM_MESSAGE = " INSERT INTO chat_room_message (chatroomid,message,senderid,createtime,sendername) VALUES (:chatroomid,:message,:userid,:createtime,:username);";
+	
 	@Override
 	public QueryMsg execute() throws Exception {
 		Map<String, Object> map = dataTable.get(0);
 
-		String userid = (String) map.get("userid");
-		String message = (String) map.get("message");
-		String sendername = (String) map.get("username");
-		String chatroomid = (String) map.get("chatroomid");
+//		String userid = (String) map.get("userid");
+//		String message = (String) map.get("message");
+//		String sendername = (String) map.get("username");
+//		String chatroomid = (String) map.get("chatroomid");
 
-		SQLQueryMsg sqlQueryMsg = new SQLQueryMsg();
-		Map<String, Object> parameters = new HashMap<String, Object>();
-		parameters.put("userid", userid);
-		parameters.put("message", message);
-		parameters.put("chatroomid", chatroomid);
-		parameters.put("sendername", sendername);
-		sqlQueryMsg.setSql(SQL_CHECK_MEMBER_ISCREATER);
-		sqlQueryMsg.setParameters(parameters);
-
-		// 执行
-		DBUtils.executeSQL(sqlQueryMsg);
-
-		String isagree = (String) sqlQueryMsg.getResultMsg().get(0)
-				.get("isagree");
+//		Map<String, Object> parameters = new HashMap<String, Object>();
+//		parameters.put("userid", userid);
+//		parameters.put("message", message);
+//		parameters.put("chatroomid", chatroomid);
+//		parameters.put("sendername", sendername);
 		
-		if ("1".equals(isagree)) {
+		SQLQueryMsg sqlQueryMsg;
+		
+		boolean isagree = checkIsBeAgree(map);
+		
+		if (isagree) {
 			sqlQueryMsg = new SQLQueryMsg();
 			sqlQueryMsg.setSql(SQL_UPDATE_CHAT_ROOM_MESSAGE);
-			sqlQueryMsg.setParameters(parameters);
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String createtime = format.format(new Date());
+			map.put("createtime", createtime);
+			sqlQueryMsg.setParameters(map);
 			DBUtils.executeSQL(sqlQueryMsg);
 		}else{
 			return setErrorResQueryMsg();
@@ -60,8 +59,23 @@ public class SendChatRoomMsg extends MainService {
 		QueryMsg resQueryMsg = new QueryMsg();
 		resQueryMsg.setResponse(true);
 		resQueryMsg.setResult("0");
+		resQueryMsg.iniDateTable();
+		resQueryMsg.getDataTable().add(map);
 		return resQueryMsg;
 
+	}
+
+	private boolean checkIsBeAgree(Map<String, Object> map) throws Exception {
+		SQLQueryMsg sqlQueryMsg = new SQLQueryMsg();
+		sqlQueryMsg.setSql(SQL_CHECK_MEMBER_ISCREATER);
+		sqlQueryMsg.setParameters(map);
+		DBUtils.executeSQL(sqlQueryMsg);
+		String isagree = (String) sqlQueryMsg.getResultMsg().get(0)
+				.get("isagree");
+		if("1".equals(isagree)){
+			return true;
+		}
+		return false;
 	}
 
 	private QueryMsg setErrorResQueryMsg() {
